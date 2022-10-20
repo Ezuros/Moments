3ea16d8d4579ee4336b5242c36c7eee0d1c77870
Daniel Martins <danielmartins888@gmail.com>
2022-10-20 13:06:42 -0300

    criação das páginas e dos services

---
+++ src/app/components/pages/new-moments/new-moments.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moments',
+  templateUrl: './new-moments.component.html',
+  styleUrls: ['./new-moments.component.css']
+})
+export class NewMomentsComponent implements OnInit {
+
+  btnText: string = 'Compartilhar'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
